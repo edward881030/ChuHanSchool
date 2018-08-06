fc88bc0dbeae7841a4fba158af3463a83c537266
huangwenqing <wudi8525hf@163.com>
2018-08-06 22:12:36 +0800

    v1.1

---
+++ views/an/course-list.js
@@ -0,0 +1,17 @@
+/**
+ *
+ Create by huangwenqing on 2018/8/4.
+ */
+
+'use strict';
+
+angular.module('courseList', []);
+
+angular.
+module('courseList').
+component('courseList', {
+    templateUrl:"an/course-list.html",
+    controller: ['$scope',
+        function HeaderNavController($scope) {
+        }]
+});
